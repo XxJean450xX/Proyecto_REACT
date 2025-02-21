6f09f2d9bca3f2cc9e6390c72399d434295afcae
Jean <jpct6666@gmail.com>
2025-02-20 22:04:54 -0500

    Publicar

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Proyecto_REACT/' // Asegúrate de que coincida con el nombre de tu repo
+  base: '/Proyecto_REACT.io/' // Asegúrate de que coincida con el nombre de tu repo
 });
