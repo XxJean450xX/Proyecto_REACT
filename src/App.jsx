f4c9f0159b8939f55bc776cc0d9b235f005cc1aa
Jean <jpct6666@gmail.com>
2025-02-20 22:17:45 -0500

    PUBLICACION

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home"
@@ -8,13 +7,11 @@ import { CartProvider } from "./context/CartContext";
 function App() {
   return (
     <CartProvider>
-      <Router>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/products" element={<Products />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
-      </Router>
     </CartProvider>
   );
 }
